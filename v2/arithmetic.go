e566ac3ed8f8e0485dcdd922de9d66fbc506e5df
mkone <mamadi.kone@dalenys.com>
2020-12-04 01:05:42 +0100

    Add isNotPrime

---
+++ v2/arithmetic.go
@@ -0,0 +1,19 @@
+package arith
+
+func IsPrime(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func IsNotPrime(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return true
+		}
+	}
+	return false
+}
---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/bamada/arith/v2
+
+go 1.15
